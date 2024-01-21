1138735755e3c0e3aee57f32fea61791a0b91fd2
Joaquin Van Messem <vanmessemjoaquin@gmail.com>
2024-01-21 18:12:20 -0300

    Agrego imagenes de los logos y termino la estética y construccion del componente de login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Login.css'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Logo from "../../images/Logo_completo.png"
 
 
 const Login = () => {
@@ -9,24 +10,28 @@ const Login = () => {
     <>
       <div className='container-sm my-5 p-3 loginContainer'>
         <div className='d-flex justify-content-center'>
-          <img src=".." alt="Royal King Hotel Logo"  className='m-3 img-thumbnail'/>
+          <img src={Logo} alt="Royal King Hotel Logo"  className='mt-3 logo'/>
         </div>
         <Form className='p-4'>
           <Form.Group className="mb-3" controlId="loginEmail">
             <Form.Label className='text-light'>Correo electrónico: </Form.Label>
-            <Form.Control type="email" placeholder="Ingrese su correo" />
+            <Form.Control type="email" placeholder="Ingrese su correo" minLength={12} maxLength={64} required/>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="loginPassword">
-            <Form.Label className='text-light'>Contraseña: </Form.Label>
-            <Form.Control type="password" placeholder="Ingrese su contraseña" />
+            <Form.Label className='text-light '>Contraseña: </Form.Label>
+            <Form.Control type="password" placeholder="Ingrese su contraseña" minLength={6} maxLength={20} required/>
           </Form.Group>
-          <p className='text-light'>¿Olvidaste tu contraseña?</p>
-          <Button variant="warning" type="submit">
+          <p className='yellowLetters'>¿Olvidaste tu contraseña?</p>
+          <Button variant="warning" type="submit" className='me-3 buttonHover yellowBG'>
             Iniciar sesión
-          </Button> <span className='text-warning'>Crear una nueva cuenta</span>
+          </Button> 
+          <span className='yellowLetters'>Crear una nueva cuenta</span>
         </Form>
-    </div>
+      </div>
+      <div className='d-flex justify-content-center mb-4'>
+      <Button variant='info' className='darkBlueBG text-light'> Volver al inicio </Button>
+      </div>
     </>
   )
 }
