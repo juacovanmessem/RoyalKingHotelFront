568f75dd38791a36ed5959e747e7bb25c34f0e63
Belén Martinez <m.belenmartinez99@gmail.com>
2024-02-04 15:36:11 -0300

    prueba de contacto, envío y recepcion de mail

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -13,9 +13,11 @@ import Container from 'react-bootstrap/Container';
 const Contacto = () => {
   const enviarMail = () =>  {
     const name = document.getElementById('nombre').value
+    const lastname = document.getElementById('lastnombre').value
     const email = document.getElementById('email').value
     const mensaje = document.getElementById('mensaje').value
-    const motivo = document.getElementById('motivo').value
+    const mensaje2 = document.getElementById('mensaje2').value
+    
 
   
     Email.send({
@@ -24,8 +26,8 @@ const Contacto = () => {
       Password : "A226829DD57847DFE1A09F6BB5BDA50EDADF",
       To : 'm.belenmartinez99@gmail.com',
       From : "m.belenmartinez99@gmail.com",
-      Subject : `¡${name} a enviado una consulta!`,
-      Body : `A traves del mail: ${email}, nos envìa el siguiente mensaje: ${mensaje} con el motivo: ${motivo}`
+      Subject : `¡${name} ${lastname} a enviado una consulta!`,
+      Body : `A traves del mail: ${email}, nos envìa el siguiente mensaje: ${mensaje} con el motivo: ${mensaje2}`
     
     }).then(); 
     const myModal = new bootstrap.Modal(document.getElementById('ModalFuncional'), {});
@@ -47,7 +49,7 @@ const Contacto = () => {
            </Form.Group>
            <Form.Group as={Col} controlId="FormGridName">
              <Form.Label className='text-light'>Apellido: </Form.Label>
-             <Form.Control type="text" placeholder="Ingrese su apellido" minLength={2} maxLength={30} required/>
+             <Form.Control type="text" id='lastname' placeholder="Ingrese su apellido" minLength={2} maxLength={30} required/>
            </Form.Group>
            <Form.Group as={Col} controlId="formGridNumero">
               <Form.Label  className='text-light'>Numero de reserva:</Form.Label>
@@ -57,8 +59,8 @@ const Contacto = () => {
 
          <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridMotivo">
-             <Form.Label className='text-light' id='motivo'>Motivo:</Form.Label>
-             <Form.Select defaultValue="">
+             <Form.Label className='text-light'>Motivo:</Form.Label>
+             <Form.Select defaultValue="" id='mensaje2'>
                <option>Olvide mi usuario y contraseña</option>
                <option>Quisiera recibir asistencia para realizar una reserva</option>
                <option>Quisiera recibir informacion sobre promociones</option>
@@ -74,7 +76,7 @@ const Contacto = () => {
          </Row>
          <Form.Group as={Col} controlId="formGridMensaje" >
             <Form.Label className='text-light'>Mensaje:</Form.Label>
-            <Form.Control type="email" placeholder="Escribe un mensaje" id='mensaje'/>
+            <Form.Control type="text" placeholder="Escribe un mensaje" id='mensaje'/>
          </Form.Group>
          <br></br>
          <Form.Group className="mb-3" id="formGridCheckbox">
